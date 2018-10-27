2ddf22f1e4b2d3e4a31c9d12ae55d3d4b157666a
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-10-27 22:01:01 +0000

    Change git clone URL.

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #pimouse_rosをgit cloneでワークスペースに持ってくる
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/citueda/pimouse_ros.git
+git clone --depth=1 https://github.com/tomohiko-nomura/pimouse_ros.git
 		#↑depth=1を指定すると最新のものだけクローンできる
 
 cd ~/catkin_ws
